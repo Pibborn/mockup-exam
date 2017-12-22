7b1914eaa8639597c6de5a55e2036f291a9a4870
Mattia Cerrato <mattia.cerrato@studenti.unito.it>
2017-12-22 15:28:51 +0100

    ex1, ex2: Remove solutions

--- ex2_classification.py
+++ ex2_classification.py
@@ -16,12 +16,6 @@ class MyNetwork(MultiLayerNetwork):
 
     def build_train(self):
         # your work here...
-        activation = tf.sigmoid(self.output)
-        pos = self.target_ph * tf.log( activation )
-        neg = (1.0 - self.target_ph) * tf.log( 1.0 - activation )
-        per_example_error = tf.reduce_sum(pos + neg,1)
-        result = -tf.reduce_mean( per_example_error )
-        self.loss_function = result
 
     def build_evaluate(self):
         super().build_evaluate()
@@ -33,19 +27,8 @@ class MyNetwork(MultiLayerNetwork):
         super().train_epoch(X_test, y_test, num_epochs=num_epochs)
 
 if __name__ == '__main__':
-    tf.reset_default_graph()
     all_data = load_data('breast-cancer-wisconsin.data')
     # remember to one-hot encode your data:
     # target = OneHotEncoder(sparse=False).fit_transform(target.reshape(-1, 1))
 
-    ###
     # your work here...
-    data = all_data[:, 1:-1]
-    target = all_data[:, -1]
-    data = preprocess_data(data)
-    target = clean_labels(target)
-    target = OneHotEncoder(sparse=False).fit_transform(target.reshape(-1, 1))
-    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=33)
-    multilayer = MyNetwork(X_train, y_train)
-    multilayer.train_epoch(X_test, y_test, num_epochs=100)
-    ###
