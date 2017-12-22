0e16b170f506cc6532e066087e593310981a2365
Mattia Cerrato <mattia.cerrato@studenti.unito.it>
2017-12-22 13:37:13 +0100

    ex2: Move the MultiLayerNetwork class to a separate file

--- ex2_classification.py
+++ ex2_classification.py
@@ -1,4 +1,4 @@
-from utils import load_data, shuffle_data
+from utils import load_data, shuffle_data, MultiLayerNetwork
 import tensorflow as tf
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
@@ -8,81 +8,35 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_iris
 ###
 
-class MultiLayerNetwork():
 
-    def __init__(self, data, target, batch_size=8, hidden_layer_sizes=[100, 30, 10],
-                 learning_rate=0.001):
-        self.learning_rate = learning_rate
-        self.data = data
-        self.target = target
-        self.num_features = np.shape(self.data)[1]
-        self.num_classes = np.shape(self.target)[1]
-        self.data_ph = tf.placeholder(tf.float32, [None, self.num_features])
-        self.target_ph = tf.placeholder(tf.float32, [None, self.num_classes])
-        self.hidden_layer_sizes = hidden_layer_sizes
-        self.batch_size = batch_size
-        self.output = None
-        self.training = None
-        self.evaluation = None
+class MyNetwork(MultiLayerNetwork):
 
-    def build_inference(self):
-        if self.output == None:
-            input_layer_weights = tf.Variable(tf.truncated_normal([self.num_features, self.hidden_layer_sizes[0]]))
-            input_layer_bias = tf.Variable(tf.constant(.1, shape=[self.hidden_layer_sizes[0]]))
-            input_layer_output = tf.matmul(self.data_ph, input_layer_weights) + input_layer_bias
-            previous_layer_output = tf.sigmoid(input_layer_output)
-            for i in range(len(self.hidden_layer_sizes) - 1):
-                temp_weights = tf.Variable(tf.truncated_normal([self.hidden_layer_sizes[i], self.hidden_layer_sizes[i+1]]))
-                temp_biases = tf.Variable(tf.constant(.1, shape=[self.hidden_layer_sizes[i+1]]))
-                hidden_layer_output = tf.sigmoid(tf.matmul(previous_layer_output, temp_weights) + temp_biases)
-                previous_layer_output = hidden_layer_output
-            output_layer_weights = tf.Variable(tf.truncated_normal([self.hidden_layer_sizes[-1], self.num_classes]))
-            output_layer_biases = tf.Variable(tf.constant(.1, shape=[self.num_classes]))
-            self.output = tf.matmul(previous_layer_output, output_layer_weights) + output_layer_biases
-
-        return self.output
+    def __init__(self, data, target):
+        super.__init__(data, target)
 
     def build_train(self):
-        if self.training == None:
-            self.loss_function = tf.nn.softmax_cross_entropy_with_logits(labels=self.target_ph, logits=self.output)
-            self.training = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.loss_function)
-        return self.training 
+        # your work here...
+        pass
 
     def build_evaluate(self):
-        if self.evaluation == None:
-            self.evaluation = tf.reduce_mean(
-                tf.cast(tf.equal(tf.argmax(self.target_ph, 1), tf.argmax(self.output, 1)), tf.float32))
-        return self.evaluation
+        super.build_evaluation()
+
+    def build_inference(self):
+        super.build_inference()
 
     def train_epoch(self, X_test, y_test, num_epochs=1):
-        if self.output == None:
-            self.build_inference()
-            self.build_train()
-            self.build_evaluate()
-            self.session = tf.Session()
-            self.session.run(tf.global_variables_initializer())
-            self.session.run(tf.local_variables_initializer())
-        num_batches = int(len(self.data) / self.batch_size)
-        for i in range(num_epochs):
-            data_train, target_train = shuffle_data(self.data, self.target)
-            for j in range(num_batches):
-                batch_data = data_train[j * self.batch_size:(j+1) * self.batch_size]
-                batch_target = target_train[j * self.batch_size:(j+1) * self.batch_size]
-                _, loss = self.session.run([self.training, self.loss_function], 
-                                 {self.data_ph: batch_data, self.target_ph: batch_target})
-            accuracy, loss, out, target = self.session.run([self.evaluation, self.loss_function, 
-                                                            self.output, self.target_ph], {self.data_ph: X_test, self.target_ph: y_test})
-            print('Epoch {}; accuracy {}'.format(i, accuracy))
+        super.train_epoch(X_test, y_test, num_epochs=num_epochs)
+
+
 
 if __name__ == '__main__':
     tf.reset_default_graph()
     all_data = load_data('breast-cancer-wisconsin.data')
-    #data, target = load_iris(True)
-    
     # remember to one-hot encode your data:
     # target = OneHotEncoder(sparse=False).fit_transform(target.reshape(-1, 1))
 
     ###
+    # your work here...
     data = all_data[:, 1:-1]
     target = all_data[:, -1]
     data = preprocess_data(data)
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import pandas
 import numpy as np
+import tensorflow as tf
+import numpy as np
 
 def load_data(path):
     dataframe = pandas.read_csv(path, header=None)
@@ -14,6 +16,72 @@ def shuffle_data(a, b):
     assert np.shape(a)[0] == np.shape(b)[0]
     p = np.random.permutation(len(a))
     return a[p], b[p]
+
+class MultiLayerNetwork():
+
+    def __init__(self, data, target, batch_size=8, hidden_layer_sizes=[100, 30, 10],
+                 learning_rate=0.001):
+        self.learning_rate = learning_rate
+        self.data = data
+        self.target = target
+        self.num_features = np.shape(self.data)[1]
+        self.num_classes = np.shape(self.target)[1]
+        self.data_ph = tf.placeholder(tf.float32, [None, self.num_features])
+        self.target_ph = tf.placeholder(tf.float32, [None, self.num_classes])
+        self.hidden_layer_sizes = hidden_layer_sizes
+        self.batch_size = batch_size
+        self.output = None
+        self.training = None
+        self.evaluation = None
+
+    def build_inference(self):
+        if self.output == None:
+            input_layer_weights = tf.Variable(tf.truncated_normal([self.num_features, self.hidden_layer_sizes[0]]))
+            input_layer_bias = tf.Variable(tf.constant(.1, shape=[self.hidden_layer_sizes[0]]))
+            input_layer_output = tf.matmul(self.data_ph, input_layer_weights) + input_layer_bias
+            previous_layer_output = tf.sigmoid(input_layer_output)
+            for i in range(len(self.hidden_layer_sizes) - 1):
+                temp_weights = tf.Variable(tf.truncated_normal([self.hidden_layer_sizes[i], self.hidden_layer_sizes[i+1]]))
+                temp_biases = tf.Variable(tf.constant(.1, shape=[self.hidden_layer_sizes[i+1]]))
+                hidden_layer_output = tf.sigmoid(tf.matmul(previous_layer_output, temp_weights) + temp_biases)
+                previous_layer_output = hidden_layer_output
+            output_layer_weights = tf.Variable(tf.truncated_normal([self.hidden_layer_sizes[-1], self.num_classes]))
+            output_layer_biases = tf.Variable(tf.constant(.1, shape=[self.num_classes]))
+            self.output = tf.matmul(previous_layer_output, output_layer_weights) + output_layer_biases
+        return self.output
+
+    def build_train(self):
+        if self.training == None:
+            self.loss_function = tf.nn.softmax_cross_entropy_with_logits(labels=self.target_ph, logits=self.output)
+            self.training = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.loss_function)
+        return self.training 
+
+    def build_evaluate(self):
+        if self.evaluation == None:
+            self.evaluation = tf.reduce_mean(
+                tf.cast(tf.equal(tf.argmax(self.target_ph, 1), tf.argmax(self.output, 1)), tf.float32))
+        return self.evaluation
+
+    def train_epoch(self, X_test, y_test, num_epochs=1):
+        if self.output == None:
+            self.build_inference()
+            self.build_train()
+            self.build_evaluate()
+            self.session = tf.Session()
+            self.session.run(tf.global_variables_initializer())
+            self.session.run(tf.local_variables_initializer())
+        num_batches = int(len(self.data) / self.batch_size)
+        for i in range(num_epochs):
+            data_train, target_train = shuffle_data(self.data, self.target)
+            for j in range(num_batches):
+                batch_data = data_train[j * self.batch_size:(j+1) * self.batch_size]
+                batch_target = target_train[j * self.batch_size:(j+1) * self.batch_size]
+                self.session.run([self.training, self.loss_function], 
+                                 {self.data_ph: batch_data, self.target_ph: batch_target})
+            accuracy = self.session.run([self.evaluation], {self.data_ph: X_test, self.target_ph: y_test})
+            print('Epoch {}; accuracy {}'.format(i, accuracy))
+
+
 
 if __name__ == '__main__':
     data = load_data('breast-cancer-wisconsin.data')
