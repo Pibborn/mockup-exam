eb480383f04cc4cc0f90d8983243e725f8eec3f0
Roberto Esposito <roberto.esposito@unito.it>
2017-12-22 16:40:13 +0100

    Updated ex2 script to match -distr branch

--- utils.py
+++ utils.py
@@ -52,9 +52,9 @@ class MultiLayerNetwork():
 
     def build_train(self):
         if self.training == None:
-            self.loss_function = tf.nn.softmax_cross_entropy_with_logits(labels=self.target_ph, logits=self.output)
+            self.loss_function = self.build_loss()
             self.training = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.loss_function)
-        return self.training 
+        return self.training
 
     def build_evaluate(self):
         if self.evaluation == None:
@@ -62,7 +62,10 @@ class MultiLayerNetwork():
                 tf.cast(tf.equal(tf.argmax(self.target_ph, 1), tf.argmax(self.output, 1)), tf.float32))
         return self.evaluation
 
-    def train_epoch(self, X_test, y_test, num_epochs=1):
+    def build_loss(self):
+        raise "Method not implemented"
+
+    def train(self, X_test, y_test, num_epochs=1):
         if self.output == None:
             self.build_inference()
             self.build_train()
@@ -76,10 +79,10 @@ class MultiLayerNetwork():
             for j in range(num_batches):
                 batch_data = data_train[j * self.batch_size:(j+1) * self.batch_size]
                 batch_target = target_train[j * self.batch_size:(j+1) * self.batch_size]
-                self.session.run([self.training, self.loss_function], 
+                self.session.run([self.training, self.loss_function],
                                  {self.data_ph: batch_data, self.target_ph: batch_target})
-            accuracy = self.session.run([self.evaluation], {self.data_ph: X_test, self.target_ph: y_test})
-            print('Epoch {}; accuracy {}'.format(i, accuracy))
+            accuracy, loss = self.session.run([self.evaluation, self.loss_function], {self.data_ph: X_test, self.target_ph: y_test})
+            print('Epoch {}; loss {}; accuracy {}'.format(i, loss, accuracy))
 
 
 
